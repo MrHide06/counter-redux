3d6db852477fbb31ff75307caf75f27b57505cad
MrHide06 <wahid.smkn10@gmail.com>
2020-04-23 19:15:40 +0700

    Penambahan

--- src/Pages/Hook.js
+++ src/Pages/Hook.js
@@ -8,7 +8,7 @@ const Hook = () => {
     const dispatch = useDispatch()
     return (
         <div>
-            {cartSelector}
+            <h1>Cart nya adalah {cartSelector}</h1>
             <Button onClick={() => dispatch(addCart())}>Add Cart</Button>
             <Button onClick={() => dispatch(minusCart())}>Minus Cart</Button>
         </div>
